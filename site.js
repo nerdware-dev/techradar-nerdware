88a4c6b9147d388a43103ad224c88513b2670c46
abendheimer <123567199+abendheimer@users.noreply.github.com>
2023-04-11 10:59:56 +0200

    Anpassung für Nerdware

---
+++ site.js
@@ -0,0 +1,9 @@
+require('./common')
+require('./images/logo.png')
+require('./images/logo-nw.png')
+require('./images/radar_legend.png')
+require('./gtm.js')
+
+const Factory = require('./util/factory')
+
+Factory().build()
